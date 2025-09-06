f0a49899384339b082dbe4754f654722df9b0fc6
Pedro Consales <109181748+Pedro-Consales@users.noreply.github.com>
2025-09-06 15:12:35 +0000

    adjustex2

--- INF_1018/lab_3/ex2.c
+++ INF_1018/lab_3/ex2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int odd_ones(unsigned int x, int *contBit1 )//Mudei o parâmetro para receber o contador na main
+int odd_ones(unsigned int x)//Mudei o parâmetro para receber o contador na main
 {
     int contadorBitsUm = 0;
     while (x != 0)
@@ -11,8 +11,6 @@ int odd_ones(unsigned int x, int *contBit1 )//Mudei o parâmetro para receber o 
         }
         x >>= 1;
     }
-
-    *contBit1 = contadorBitsUm;
 
     int verfOdd_Con = contadorBitsUm & 1;
 
@@ -28,30 +26,11 @@ int odd_ones(unsigned int x, int *contBit1 )//Mudei o parâmetro para receber o 
 
 
 
-int main(void)
-{
-    unsigned int x = 7;
-    int contBit1 = 0;
-
-    int odd_con = odd_ones(x, &contBit1);
-
-    printf("Número verificado foi: %u \n\n", x);
-
-    printf("O contador de bits 1 foi: %d --> ", contBit1);
-    if(odd_con == 1)
-    {
-        printf("É Ímpar\n\n");
-    }
-    else
-    {
-        printf("É par\n\n");
-    }
-    
-    printf("Num contador de Bits em binário: %08b\n", contBit1);
-
-    return 0;
+int main() {
+  printf("%x tem numero %s de bits\n",0x01010101,odd_ones(0x01010101) ? "impar":"par");
+  printf("%x tem numero %s de bits\n",0x01030101,odd_ones(0x01030101) ? "impar":"par");
+  return 0;
 }
-
 
 
 //Exemplo:
