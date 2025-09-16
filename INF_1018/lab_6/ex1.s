65541f60444cec02aa8a240f34d31b3fd58b3c63
Pedro Consales <109181748+Pedro-Consales@users.noreply.github.com>
2025-09-16 21:24:49 +0000

    Lab_6

--- INF_1018/lab_6/ex1.s
+++ INF_1018/lab_6/ex1.s
@@ -10,7 +10,7 @@ int main() {
 
 .data
 nums:  .int  10, -21, -30, 45
-Sf:  .string "%d\n"    # string de formato para printf
+Sf:  .string "%d\n"    /* string de formato para printf */
 
 .text
 .globl  main
@@ -25,31 +25,38 @@ main:
   movq    %r12, -16(%rbp)
 /********************************************************/
 
-#for (i = 0, p = nums; i != 4; i++, p++)
-# i = 0
-# p = nums
-# while i != 4
-# i++
-# p++
-  movl  $0, %ebx  /* ebx = 0; */
-  movq  $nums, %r12  /* r12 = &nums */
+/* for (i = 0, p = nums; i != 4; i++, p++) */
+/* i = 0 */
+/* p = nums */
+
+/* while i != 4 */
+/* i++ */
+/* p++ */
+
+  movl  $0, %ebx  /* ebx = 0; 0 é um numero inteiro de 4 bytes = 32 bits, então eu pego o 0 e guardo no registrador de 32bits */
+  movq  $nums, %r12  /* r12 = &nums; movQ pega o endereçodo primeiro elemento do vetor e guarda com 8 bytes no regsitrador r12 */
 
 L1:
-  cmpl  $4, %ebx  /* if (ebx == 4) ? */
-  je  L2          /* goto L2 */
+  cmpl  $4, %ebx  /* compara ebx com 4 (a comparação ainda não sabemos, é definida na linha seguinte) */
+  je  L2          /* goto L2; o que ta sendo comparado antes -> je = jump (if) equal para L2 e executa o bloco L2. */
 
-  movl  (%r12), %eax    /* eax = *r12 */
+  /* Se L2 Falso conitnua linha a linha, se não, vai para o fim do código. */
+  /* Nesse caso como estamos fazendo um for, o if é para encerrar qunado o loop atinge a quantidade máxima */
 
+  /* Se falso, continua loop */
+  movl  (%r12), %eax    /* eax = *r12; (%r12) -> pega o conteúdo do que r12 guarda (endereço do primeiro elemento de nums) 
+                           movl -> mova para %eax (registrador de 4bytes = 32 bits) esse valor
+                           logo em eax vamos ter o valor do priemiro elemento do vetor no i = 0 -> vetor[i] */
 /*************************************************************/
-/* este trecho imprime o valor de %eax (estraga %eax)  */
-  movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
-  movl    %eax, %esi   /* segundo parametro  (inteiro) */
-  call  printf       /* chama a funcao da biblioteca */
+/* este trecho imprime o valor de %eax (estraga %eax)  */ 
+  movq    $Sf, %rdi    /* primeiro parametro (ponteiro); quem é Sf? */
+  movl    %eax, %esi   /* segundo parametro  (inteiro); eax é o conteúdo do vetor[i] e guardo esse conteúdo no registrador de 8bytes esi */
+  call  printf         /* chama a funcao da biblioteca; por que printamos eax e não o conteúdo do potneiro? Pois no print o registrador é destruido, então tecnicamente eax funciona com uma epécie de buffer */
 /*************************************************************/
 
-  addl  $1, %ebx  /* ebx += 1; */
-  addq  $4, %r12  /* r12 += 4; */
-  jmp  L1         /* goto L1; */
+  addl  $1, %ebx  /* ebx += 1; incrementa 1 ao ebx que é o contador */
+  addq  $4, %r12  /* r12 += 4; anda uma casa do vetor + 4 bytes na memória */
+  jmp  L1         /* goto L1; Volta o loop */
 
 L2:  
 /***************************************************************/
---
+++ INF_1018/lab_6/ex3.s
@@ -0,0 +1,90 @@
+/*
+int nums[] = {10, -21, -30, 45};
+int main() {
+  int i, *p;
+  for (i = 0, p = nums; i != 4; i++, p++)
+    if ((*p % 2) == 0)
+      printf("%d\n", *p);
+  return 0;
+}
+*/
+
+/*
+Fazer simplificação do código
+
+
+int nums[] = {10, -21, -30, 45};
+
+int main(void) {
+  int i = 0;
+  int *p = nums;
+
+  while (i < 4) {
+    if ((*p % 2) == 0) {
+      printf("%d\n", *p);
+    }
+    i++;
+    p++;
+  }
+  return 0;
+}
+
+*/
+
+
+.data
+nums:  .int  10, -21, -30, 45
+Sf:  .string "%d\n"    /* string de formato para printf */
+
+.text
+.globl  main
+main:
+
+/*prólogo*/
+pushq   %rbp        /* salva o valor antigo de %rbp na pilha          */
+movq    %rsp, %rbp  /* cria um novo frame base: %rbp = %rsp           */
+subq    $16, %rsp   /* reserva 16 bytes no stack frame da função      */
+movq    %rbx, -8(%rbp)   /* salva o valor de %rbx em variavel local   */
+movq    %r12, -16(%rbp)  /* salva o valor de %r12 em variavel local   */
+
+
+
+
+movl $0, %ebx /*i=0*/
+movq $nums, %r12 /*guardador do erndereço do primeiro elemento*/
+
+loop:
+
+    cmpl $4, %ebx
+    jge break 
+
+    movl (%r12), %eax
+    movl %eax, %ecx
+    and $0x01, %ecx
+
+    cmpl $0, %ecx
+    jne notcond           /* ((*p % 2) == 0) */
+    
+    /* printf("%d\n", *p); */
+    movl   (%r12), %esi    # carrega *p em %esi (segundo argumento)
+    movq   $Sf, %rdi       # primeiro argumento = endereço da string "%d\n"
+    xor    %eax, %eax      # limpa %eax
+    call   printf
+
+    notcond:
+          
+
+    addl $1, %ebx
+    addq $4, %r12
+    jmp loop
+
+
+
+break:
+
+      /* retorno 0 + epílogo */
+      movl   $0, %eax    # return 0
+      movq   -8(%rbp), %rbx
+      movq   -16(%rbp), %r12
+      leave
+      ret
